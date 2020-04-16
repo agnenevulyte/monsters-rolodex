d833dc3a3ba578c63faa815c29b8c7e9f92afdc6
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2020-04-16 20:26:24 +0100

    Updates

---
+++ precache-manifest.7c39b567d1634071722a63d46cf35e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f614972c157e00560d9904d4a3f69038",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc5fb79176ea001dae11",
+    "url": "/monsters-rolodex/static/css/main.54cd0841.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5fb79176ea001dae11",
+    "url": "/monsters-rolodex/static/js/main.2745b6ee.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
